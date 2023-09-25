40c347392e974fc600678fed2034a77bdb995c4b
MIAN MOHAMMAD RASSEL <145997251+mmrassel@users.noreply.github.com>
2023-09-25 20:51:42 +0600

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[8];
+            for(int i =0;i<8;i++)
+            {
+                a[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+
+            int x = a[0], y = a[0];
+
+            for(int i = 1; i<8; i++)
+            {
+                if (a[i] > x)
+                {
+                    x = a[i];
+                }
+                if (a[i] < y)
+                {
+                    y = a[i];   
+                }
+            }
+
+            Console.WriteLine($"Max Value is: {x}"); ;
+            Console.WriteLine($"Min Value is: {y}"); ;
+            Console.ReadLine();            
+        }
+    }
+}
